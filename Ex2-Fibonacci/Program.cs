858cd59f184f3cd0b95a86169b8af351c0e77e0e
Francine-Oliveira <olive.fran86@gmail.com>
2023-03-23 21:55:10 -0300

    resposta 2

--- Ex2-Fibonacci/Program.cs
+++ Ex2-Fibonacci/Program.cs
@@ -3,7 +3,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int termo1 = 0, termo2 = 1;
+        int termo3 = 0;
+        string str = new('=', 28);
+
+        Console.WriteLine(str);
+        Console.WriteLine(" * Sequência de Fibonacci *");
+        Console.WriteLine(str);
+        Console.Write("Digite um número: ");
+        int valor = Convert.ToInt32(Console.ReadLine());
+
+        while (valor > termo3)
+        {
+            termo3 = termo1 + termo2;
+            termo1 = termo2;
+            termo2 = termo3;
+        }
+        if (valor == 0 || valor == 1 || valor == termo3)
+        {
+            Console.WriteLine("O número {0} faz parte da sequência de Fibonacci.", valor);
+        }
+        else
+        {
+            Console.WriteLine("O número {0} não faz parte da sequência de Fibonacci.", valor);
+        }
+
+        Console.ReadKey();
     }
 }
 
